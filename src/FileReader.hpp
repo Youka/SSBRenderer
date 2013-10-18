54b9a42264cfa950d5d97fb2074e4a2af9b72104
Youka <spanknebel.borken@t-online.de>
2013-10-18 16:59:08 +0200

    began ssb parser with file lines iteration

---
+++ src/FileReader.hpp
@@ -0,0 +1,81 @@
+#pragma once
+
+#include <windows.h>
+#include <string>
+#include <algorithm>
+
+class FileReader{
+    private:
+        // File handle
+        HANDLE file;
+        // Read buffer
+        char buffer[4096];
+        char* buffer_start = buffer + sizeof(buffer), *const buffer_end = buffer_start;
+    public:
+        // Constructors
+        FileReader()
+        : file(INVALID_HANDLE_VALUE){}
+        FileReader(std::string& filename)
+        : file(CreateFileA(filename.c_str(), GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL)){}
+        FileReader(std::wstring& filename)
+        : file(CreateFileW(filename.c_str(), GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL)){}
+        FileReader(FileReader& reader)
+        : file(reader.file){
+            reader.file = INVALID_HANDLE_VALUE;
+        }
+        // Assign operator
+        FileReader& operator =(FileReader& reader){
+            if(this->file != INVALID_HANDLE_VALUE)
+                CloseHandle(this->file);
+            this->file = reader.file;
+            this->buffer_start = this->buffer_end;
+            reader.file = INVALID_HANDLE_VALUE;
+            return *this;
+        }
+        // Destructor
+        virtual ~FileReader(){
+            if(this->file != INVALID_HANDLE_VALUE)
+                CloseHandle(this->file);
+        }
+        // Check file state
+        operator bool(){
+            return this->file != INVALID_HANDLE_VALUE;
+        }
+        // Reset file pointer
+        void reset(){
+            if(this->file != INVALID_HANDLE_VALUE){
+                SetFilePointer(this->file, 0, 0, FILE_BEGIN);
+                this->buffer_start = this->buffer_end;
+            }
+        }
+        // Read one line from file
+        bool getline(std::string& line){
+            // Clear old line content
+            line.clear();
+            // Fill line content
+            while(true){
+                // Check for buffer content
+                if(buffer_start == buffer_end){
+                    // Fill buffer
+                    DWORD read;
+                    if(!ReadFile(this->file, this->buffer, sizeof(this->buffer), &read, NULL) || read == 0)
+                        // Error or EOF
+                        break;
+                    this->buffer_start = this->buffer;
+                }
+                // Search newline
+                char* newline = std::find(this->buffer_start, this->buffer_end, '\n');
+                // Append current line
+                if(newline == this->buffer_end){
+                    line.append(this->buffer_start, this->buffer_end);
+                    this->buffer_start = this->buffer_end;
+                }else{
+                    line.append(this->buffer_start, newline);
+                    this->buffer_start = newline + 1;
+                    return true;
+                }
+            }
+            // Read anything?
+            return !line.empty();
+        }
+};
--- src/SSBParser.cpp
+++ src/SSBParser.cpp
@@ -1,9 +1,32 @@
 #include "SSBParser.hpp"
+#if defined _WIN32
+    #include "textconv.hpp"
+    #include "FileReader.hpp"
+#else
+    #include <fstream>
+#endif
 
 SSBParser::SSBParser(std::string& script, bool warnings){
     this->parse(script, warnings);
 }
 
 void SSBParser::parse(std::string& script, bool warnings){
-
+    // File line buffer
+    std::string line;
+    // File reading
+#if defined _WIN32   // Windows
+    std::wstring scriptW = utf8_to_utf16(script);
+    FileReader file(scriptW);
+    if(file)
+        while(file.getline(line)){
+#else   // Unix
+    std::ifstream file(script);
+    if(file)
+        while(std::getline(file, line)){
+#endif
+            #pragma message "Evaluate file line"
+        }
+    // File couldn't be read
+    else if(warnings)
+        throw std::string("Script couldn't be read: ") + script;
 }
