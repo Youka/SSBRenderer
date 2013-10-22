6d1f683f53ddebfcab9a064ab70ee141faef4bac
Youka <spanknebel.borken@t-online.de>
2013-10-22 20:16:46 +0200

    changed license; added some documentation; added rendering test

--- README.md
+++ README.md
@@ -3,3 +3,18 @@
 <td><img src=src/res/logo.bmp /></td>
 <td><b>SSBRenderer</b> is a <a href=http://en.wikipedia.org/wiki/Plug-in_%28computing%29>plugin</a> for <a href=http://en.wikipedia.org/wiki/Frameserver>frameservers</a> on <a href=http://en.wikipedia.org/wiki/Microsoft_Windows>Windows</a> & <a href=http://en.wikipedia.org/wiki/Unix>Unix</a> platforms to <a href=http://en.wikipedia.org/wiki/Rendering_%28computer_graphics%29>render</a> with SSB (<i>Substation Beta</i>) data on <a href=http://en.wikipedia.org/wiki/Film_frame>frames</a> of video <a href=http://en.wikipedia.org/wiki/Streaming_media>streams</a>.</td>
 </tr></table>
+
+### Build
+Comes after further development.
+
+### Example
+Comes after further development.
+
+### Contact
+In future, there'll be an IRC channel [#SSB @ freenode](irc://#SSB@chat.freenode.net/).
+
+### Contributing
+Currently no help is needed. After the first binary release it's going to change.
+
+### License
+This software is under zlib license. For details, read <b>LICENSE.txt<b>.
--- src/Renderer.cpp
+++ src/Renderer.cpp
@@ -1,5 +1,6 @@
 #include "Renderer.hpp"
 #include "SSBParser.hpp"
+#include <cairo.h>
 
 Renderer::Renderer(int width, int height, Colorspace format, std::string& script, bool warnings)
 : width(width), height(height), format(format), ssb(SSBParser(script, warnings).data()){}
@@ -12,4 +13,17 @@ void Renderer::set_target(int width, int height, Colorspace format){
 
 void Renderer::render(unsigned char* image, int pitch, signed long long start_ms, signed long long end_ms){
 #pragma message "Implent SSB rendering"
+    if(this->format == Colorspace::BGRA){
+        cairo_surface_t* surface = cairo_image_surface_create_for_data(image, CAIRO_FORMAT_ARGB32, this->width, this->height, this->width << 2);
+        cairo_t* ctx = cairo_create(surface);
+        cairo_set_source_rgba(ctx, 1, 1, 0, 0.75);
+        cairo_move_to(ctx, 0, 0);
+        cairo_line_to(ctx, 200, 200);
+        const double dashes[3] = {1,5,3};
+        cairo_set_dash(ctx, dashes, sizeof(dashes) / sizeof(*dashes), 0.0L);
+        cairo_set_line_width(ctx, 5);
+        cairo_stroke(ctx);
+        cairo_destroy(ctx);
+        cairo_surface_destroy(surface);
+    }
 }
